3518ed03b61f094065dd3f2d0eafa28051293bad
Sergey Zakharov <zakharov.nsg@gmail.com>
2026-04-20 03:26:34 +0500

    Initial Commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Сергей Захаров on 20.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
