44853024b73b649f684f39cbe1e7359953cbd3b9
Sergey Zakharov <zakharov.nsg@gmail.com>
2026-04-26 03:26:56 +0500

    initial commit

---
+++ Flashzilla/Card.swift
@@ -0,0 +1,15 @@
+//
+//  Card.swift
+//  Flashzilla
+//
+//  Created by Сергей Захаров on 26.04.2026.
+//
+
+import Foundation
+
+struct Card: Codable {
+    var prompt: String
+    var answer: String
+
+    static let example = Card(prompt: "Who played the 13th Doctor in Doctor Who?", answer: "Jodie Whittaker")
+}
